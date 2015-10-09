54f219a98eeeb685cf417ff1390cb95e4681f6fe
yuanpx <1019542257@qq.com>
2015-10-09 08:30:05 -0400

    add all

--- bison/absyn.h
+++ bison/absyn.h
@@ -20,3 +20,18 @@ typedef struct A_efield_ *A_efield;
 typedef struct A_efieldList *A_efieldList;
 
 typedef enum {A_plusOp, A_minusOp, A_timesOp, A_divideOp, A_eqOp, A_neqOp, A_ltOp, A_leOp, A_gtOp, A_geOp} A_oper;
+
+
+
+A_var A_Simple_Var(A_pos pos, S_symbol sym);
+A_var A_FieldVar(A_pos pos, A_var var, S_symbol sym);
+A_var A_SubscriptVar(A_pos pos, A_var var, A_exp exp);
+
+A_exp A_VarExp(A_pos pos, A_var var);
+A_exp A_NilExp(A_pos pos);
+A_exp A_IntExp(A_pos pos, int i);
+A_exp A_StringExp(A_pos pos, string s);
+A_exp A_CallExp(A_pos pos, S_symbol func, A_expList args);
+A_exp A_OpExp(A_pos pos, A_oper oper, A_exp left, A_exp right);
+
+
