222c742f054847e5e843c1e9e85d860039ead823
yuanpx <1019542257@qq.com>
2015-10-09 07:37:10 -0700

    add all

--- bison/absyn.h
+++ bison/absyn.h
@@ -33,5 +33,31 @@ A_exp A_IntExp(A_pos pos, int i);
 A_exp A_StringExp(A_pos pos, string s);
 A_exp A_CallExp(A_pos pos, S_symbol func, A_expList args);
 A_exp A_OpExp(A_pos pos, A_oper oper, A_exp left, A_exp right);
+A_exp A_RecordExp(A_pos pos, S_symbol typ, A_efieldList fields);
+A_exp A_SeqExp(A_pos pos, A_expList seq);
+A_exp A_AssignExp(A_pos, A_var var, A_exp exp);
+A_exp A_IfExp(A_pos pos, A_exp test, A_exp then, A_exp elsee);
+A_exp A_While(A_pos pos, A_exp test, A_exp body);
+A_exp A_Break(A_pos pos);
+A_exp A_ForExp(A_pos pos, S_symbol var, A_exp lo, A_exp hi, A_exp body);
 
+A_exp A_LetExp(A_pos pos, A_decList decs, A_exp body);
+A_exp A_ArrayExp(A_pos pos, S_symbol typ, A_exp size, A_exp init);
 
+A_dec A_FunctionDec(A_pos pos, A_fundecList function);
+A_dec A_VarDec(A_pos pos, S_symbol var, S_symbol typ, A_exp init);
+A_dec A_TypeDec(A_pos pos, A_nametyList type);
+
+A_ty A_NameTy(A_pos pos, S_symbol name);
+A_ty A_RecordTy(A_pos pos,A_nametyList type);
+
+A_field A_Field(A_pos pos, S_symbol name);
+A_fieldList A_FieldList(A_field head, A_fieldList tail);
+A_expList A_ExpList(A_exp head, A_expList tail);
+A_fundec A_Fundec(A_pos pos, S_symbol name, A_fieldList params, S_symbol result, A_exp body);
+A_fundecList A_FundecList(A_fundec head, A_fundecList tail);
+A_decList A_DecList(A_dec head, A_decList tail);
+A_namety A_Namety(S_symbol name, A_ty ty);
+A_nametyList A_NametyList(A_namety head, A_nametyList tail);
+A_efield A_Efield(S_symbol name, A_exp exp);
+A_efieldList A_EfieldList(A_efield head, A_efieldList tail);
